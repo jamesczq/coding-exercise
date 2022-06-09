6fa6abb6879f553337981ac05c6cd3c09120739b
jamesczq <jamesczq@gmail.com>
2022-06-09 00:16:41 -0400

    Add two_sum.py and filemaster

---
+++ two_sum.py
@@ -0,0 +1,63 @@
+"""
+Two Sum
+
+Given an array of integers nums and an integer target, return indices of the 
+two numbers such that they add up to target.
+
+You can assume that each input would have exactly one solution, and you may 
+not use the same element twice.
+
+Created on Wed Jun 08 2022
+"""
+from typing import List, Tuple, Union
+import argparse
+
+def two_sum_v1(nums: List[int], target: int) -> Union[Tuple, None]:
+    visited = {nums[0]: 0}
+    for i in range(1, len(nums)):
+        x = nums[i]
+        y = target - x
+        if y in visited:
+            return (x, y)
+        else:
+            visited[x] = i 
+    return None
+
+def two_sum_v2(nums: List[int], target: int) -> Union[Tuple, None]:
+    #nums_hash = {num:i for i,num in enumerate(nums)}
+    arr = sorted(nums)
+    i, j = 0, len(arr) - 1
+    while i < j:
+        mysum = arr[i] + arr[j]
+        if mysum < target:
+            i += 1
+        elif mysum  > target:
+            j -= 1
+        else:
+            return (arr[i], arr[j])
+    return None
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', "--version", type=int, default=1)
+    parser.add_argument('--nums', nargs="+", type=int)
+    parser.add_argument('--target', type=int)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    if args.version == 1:
+        ans = two_sum_v1(args.nums, args.target)
+    else:
+        ans = two_sum_v2(args.nums, args.target)
+    return ans
+
+if __name__ == "__main__":
+    ans = main()
+    if ans:
+        x, y = ans[0], ans[1]
+        print(x, y)
+    else: 
+        print("No such two numbers found!")
+
