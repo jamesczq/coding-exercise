8969edab003eb1b915011a9c5936bc1de105176a
jamesczq <jamesczq@gmail.com>
2022-08-12 17:12:26 -0400

    Add two more pys

---
+++ programs/move_element_to_end.py
@@ -0,0 +1,39 @@
+"""
+Given an array of integers A and an integer k, move all A[i] that equal k to 
+the end of A. 
+
+Perform this task in place; you don't need to maintain the order of the other
+integers in the original array.
+
+Example:
+array = [2, 1, 2, 2, 2, 3, 4, 2]
+toMove = 2
+output: [1, 3, 4, 2, 2, 2, 2, 2]
+
+Created on Fri Aug 05, 2022
+"""
+from typing import List 
+
+def move(array: List[int], toMove: int) -> List[int]:
+    # Use two-pointer technique
+    i, j = 0, len(array)-1
+    while i < j:
+        if array[i] == toMove and array[j] == toMove:
+            j -= 1
+        if array[i] == toMove and array[j] != toMove:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
+        if array[i] != toMove:
+            i += 1
+    return array 
+
+def main():
+    arr = [2, 5, 1, 5, 2, 5, 3, 4, 2, 5, 5]
+    toMove = 5
+    str_out = f"Moving all {toMove}s to the end of {arr}\n gives"
+    str_out += f"{move(arr, toMove)}"
+    print(str_out)
+
+if __name__ == "__main__":
+    main()
---
+++ programs/three_sum_v2.py
@@ -0,0 +1,42 @@
+"""
+Find all triplets in an array that sum up to a given target, using 
+two-pointer technique.
+
+Example: 
+array = [12, 3, 1, 2, -6, 5, -8, 6]
+target = 0
+output: [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+
+Time: O(n^2)
+Space: O(n)
+
+Created on Fri Aug 12, 2022
+"""
+from typing import List
+
+def find_triplets(array: List[int], target: int) -> List[List[int]]:
+    arr_out = []
+    array.sort()
+    for i in range(len(array) - 2):
+        left, right = i+1, len(array)-1
+        while left < right:
+            tri_sum = array[i] + array[left] + array[right]
+            if tri_sum < target:
+                left += 1
+            elif tri_sum > target:
+                right -= 1
+            else: # tri_sum == target 
+                arr_out.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+    return arr_out 
+
+def main():
+    arr = [12, 3, 1, 2, -6, 5, -8, 6]
+    target = 0
+    str_out = f"Triplets in {arr} that sum up to {target} are:\n"
+    str_out += f"{find_triplets(arr, target)}"
+    print(str_out)
+
+if __name__ == "__main__":
+    main()
